19c13ae5c8f6838517cece4bde75060fb80aa3e0
Luke Olende <olendeluke2k@gmail.com>
2026-01-22 13:53:18 +0300

    Implement update and delete functions

--- main.py
+++ main.py
@@ -16,3 +16,22 @@ def add_setting(settings, key_value_pair):
 
     settings[key] = value
     return f"Setting '{key}' added with value '{value}' successfully!"
+
+def update_setting(settings, key_value_pair):
+    key, value = key_value_pair
+
+    key = str(key).lower()
+    value = str(value).lower()
+
+    if key not in settings:
+        return f"Setting '{key}' does not exist! Cannot update a non-existing setting."    
+    settings[key] = value
+    return f"Setting '{key}' updated to '{value}' successfully!"
+
+def delete_setting(settings, key):
+    key = str(key).lower()
+
+    if key not in settings:
+        return "Setting not found!"
+    del settings[key]
+    return f"Setting '{key}' deleted successfully!"
